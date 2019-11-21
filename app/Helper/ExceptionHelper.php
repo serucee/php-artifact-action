e36e0aef531d537d1ff46665a36dcfbd5d780cc4
serucee <sebastian.russmann@wirecard.com>
2019-11-21 15:40:17 +0100

    Improve doc

--- app/Helper/ExceptionHelper.php
+++ app/Helper/ExceptionHelper.php
@@ -7,7 +7,7 @@ namespace ArtifactCreation\Helper;
 use Exception;
 
 /**
- * Wrapper for exceptions
+ * Wrapper for handling exceptions
  *
  * Class ExceptionHelper
  * @package ArtifactCreation\Helper
@@ -21,7 +21,7 @@ class ExceptionHelper
      * @param string $context
      * @param Exception $error
      */
-    public static function dieWithError($context,Exception $error) {
+    public static function dieWithError($context, Exception $error) {
         $msg = sprintf(
             'Could not execute %s:: %s',
             $context,
