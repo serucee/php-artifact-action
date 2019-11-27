020f315a1a73040884d2c646d658dc8d34e87802
serucee <sebastian.russmann@wirecard.com>
2019-11-27 14:15:35 +0100

    Fix entrypoint error logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -l
 
 # Exit on failure
-#set -e
+set -e
 
 echo "Calling php script"
 php /usr/local/bin/src/main.php
 EXIT_CODE=$?
 
-if (( "$EXIT_CODE" != "0" )); then
+if ! [[ "$EXIT_CODE" == 0 ]]; then
     echo "Script execution ended with error :: $EXIT_CODE"
     exit 1
 fi
