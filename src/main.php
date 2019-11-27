e72a9baf5bcbd47f1d702d086bad5917d0a3ea37
serucee <sebastian.russmann@wirecard.com>
2019-11-27 12:31:06 +0100

    Improve cs

--- src/main.php
+++ src/main.php
@@ -13,6 +13,7 @@ require __DIR__ . '/../vendor/autoload.php';
 // Currently the configuration filename is hardcoded
 // If necessary it can be a parameter passed to this file
 $fullyQualifiedConfigurationFileName = '/github/workspace/.github/artifact-configuration.json';
+
 try {
     $parserJson = new FileParserJson($fullyQualifiedConfigurationFileName);
     $configuration = new Configuration($parserJson);
