b4239e8c1d124fa6882643f1a16ab946b00ef605
serucee <sebastian.russmann@wirecard.com>
2019-11-27 16:24:16 +0100

    Improve array helper tests

--- tests/ArrayHelperTest.php
+++ tests/ArrayHelperTest.php
@@ -23,82 +23,79 @@ class ArrayHelperTest extends \Codeception\Test\Unit
     protected $nullValueArray;
 
     /**
-     * Set arrays for later usage
-     *
-     * @since 0.0.1
-     */
-    protected function _before()
-    {
-        $this->validArray = [
-            'key0' => 'test',
-            'abc' => ['hello', 'world'],
-            self::KEY => self::KEY_VALUE
-        ];
-
-        $this->nullValueArray = [
-            'key0' => 'test',
-            'abc' => ['hello', 'world'],
-            self::KEY
-        ];
-    }
-
-    // tests
-
-    /**
-     * Test value returned by key
-     *
-     * @since 0.0.1
-     */
-    public function testValueByKeyReturned()
-    {
-        $value = ArrayHelper::valueByKey($this->validArray, self::KEY);
-        $this->assertEquals(self::KEY_VALUE, $value);
-    }
-
-    /**
-     * Test value returned by key with required parameter
-     *
-     * @since 0.0.1
-     */
-    public function testValueByKeyReturnedWithRequiredParameter()
-    {
-        $value = ArrayHelper::valueByKey($this->validArray, self::KEY, true);
-        $this->assertEquals(self::KEY_VALUE, $value);
-    }
-
-    /**
-     * Test default value returned if key doesn't exist
-     *
-     * @since 0.0.1
-     */
-    public function testNullReturnedAsFallback()
-    {
-        $value = ArrayHelper::valueByKey($this->validArray, 'non-existing-key');
-        $this->assertEquals(null, $value);
-    }
-
-    /**
-     * Test passed default value returned if key doesn't exist
-     *
-     * @since 0.0.1
-     */
-    public function testPassedDefaultValueReturned()
-    {
-        $defaultValue = 'default-value';
-        $value = ArrayHelper::valueByKey($this->validArray, 'non-existing-key', false, $defaultValue);
-        $this->assertEquals($defaultValue, $value);
-    }
-
-    /**
      * Test if exception is raised on a missing required parameter
      *
      * @doesNotPerformAssertions
+     * @small
      *
      * @since 0.0.1
      */
     public function testExpectedExceptionIsRaised()
     {
+        $nullValueArray = [
+            'key0' => 'test',
+            'abc' => ['hello', 'world'],
+            self::KEY
+        ];
+
         $this->expectException(MissingParameterException::class);
-        ArrayHelper::valueByKey($this->nullValueArray, self::KEY, true);
+        ArrayHelper::valueByKey($nullValueArray, self::KEY, true);
     }
+
+    /**
+     * @param $array
+     * @param $key
+     * @param $required
+     * @param $default
+     * @param $expected
+     *
+     * @dataProvider valueByKeyDataProvider
+     * @small
+     *
+     * @throws MissingParameterException
+     * @since 0.0.1
+     */
+    public function testValueByKey($array, $key, $required, $default, $expected)
+    {
+        $result = ArrayHelper::valueByKey($array, $key, $required, $default);
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * Data provider for testValueByKey()
+     *
+     * @since 0.0.1
+     */
+    public function valueByKeyDataProvider()
+    {
+        $validArray = [
+            'key0' => 'test',
+            'abc' => ['hello', 'world'],
+            self::KEY => self::KEY_VALUE
+        ];
+
+         yield   'Value by key returned'  => [
+             $validArray,
+             self::KEY,
+             false,
+             null,
+             self::KEY_VALUE
+         ];
+
+         yield   'Value by key returned with required parameter' => [
+             $validArray,
+             self::KEY,
+             true,
+             null,
+             self::KEY_VALUE
+         ];
+
+         yield   'Passed default value returned if key-value not exists' => [
+             $validArray,
+             'non existing key',
+             false, 'fallbackValue',
+             'fallbackValue',
+         ];
+    }
+
 }
