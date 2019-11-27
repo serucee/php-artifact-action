8dc487ec69a34e471f3618876a15ecf264704c93
serucee <sebastian.russmann@wirecard.com>
2019-11-27 14:01:14 +0100

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ echo "Calling php script"
 php /usr/local/bin/src/main.php
 EXIT_CODE=$?
 
-if (( EXIT_CODE != 0 )); then
+if (( "$EXIT_CODE" != "0" )); then
     echo "Script execution ended with error :: $EXIT_CODE"
     exit 1
 fi
