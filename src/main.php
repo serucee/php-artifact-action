01bd8aa6ce35198f10bc93772dc87955302e3703
serucee <sebastian.russmann@wirecard.com>
2019-11-27 12:26:56 +0100

    Fix paths

--- src/main.php
+++ src/main.php
@@ -9,10 +9,11 @@ use ArtifactCreation\Helper\FileParserJson;
 use ArtifactCreation\Helper\Runner;
 use Exception;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 // Currently the configuration filename is hardcoded
 // If necessary it can be a parameter passed to this file
 $fullyQualifiedConfigurationFileName = '/github/workspace/.github/artifact-configuration.json';
+$runner = new Runner();
 try {
     $parserJson = new FileParserJson($fullyQualifiedConfigurationFileName);
     $configuration = new Configuration($parserJson);
